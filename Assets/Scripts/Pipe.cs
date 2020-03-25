b0fbe59b1c804f86634028e9b38cc94096f8174f
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-03-25 14:26:22 +0530

    Initial Commit

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public float speed = 10f; 
+
+    void Update()
+    {
+        // We will move towards left [not moving the camera]
+        // Only change in x-axis
+        transform.position -= new Vector3(speed * Time.deltaTime, 0,0);
+    }
+}
