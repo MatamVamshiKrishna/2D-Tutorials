2875a95c8257aab8bb0070ba86a006f7c2b2da3d
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-03-25 14:54:28 +0530

    Add sprites, bird movement and animations

---
+++ Assets/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float Speed = 100.0f;
+    Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody2D.velocity = Vector2.up * Time.deltaTime * Speed; 
+        }
+    }
+}
